979f3a6219276d8d261181c4bebc52b806eaaa86
ausmaster <cooljazzdrummer@cox.net>
2024-05-30 01:08:51 -0700

    Error.

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from __future__ import annotations
 from vaultlib.api import NVDFetch  # pylint: disable=E0401
 from vaultlib.argparser import VaultArgumentParser  # pylint: disable=E0401
 from vaultlib.mongo import VaultMongoClient  # pylint: disable=E0401
-from vaultlib.config import VaultConfig  # pylint: disable=E0401  # pylint: disable=E0401
-from vaultlib.utils import BColors as C  # pylint: disable=E0401
+from vaultlib.config import VaultConfig  # pylint: disable=E0401
+from vaultlib.utils import BColors as Colors  # pylint: disable=E0401
 
 NVD_API: NVDFetch | None = None
 VAULT_MONGO: VaultMongoClient | None = None
+C: Colors = Colors()
 
 
 def initial_load() -> None:
@@ -19,7 +20,6 @@ def initial_load() -> None:
 
     :return: None
     """
-    # pylint: disable=E1120
     print("Dropping CVEs collection... ", end="")
     VAULT_MONGO.get_default_database().drop_collection("cves")
     print(C.success_msg("Dropped."))
