c44124cd7aa6c5abce8f80e96595610db7a87e73
ausmaster <cooljazzdrummer@cox.net>
2024-06-22 23:24:14 -0700

    Improvements to ML algorithm.

--- query.py
+++ query.py
@@ -4,75 +4,99 @@ This is the script with functions to query certain items from the MongoDB
 from __future__ import annotations
 
 from operator import itemgetter
+from typing import Callable
 
 import nltk
 from nltk.tokenize import word_tokenize
 from rapidfuzz.fuzz import WRatio
 
 # pylint: disable=E0401,E0611
-from vaultlib import VaultArgumentParser, VaultConfig, VaultMongoClient, s_print, VAULT_MONGO
+from vaultlib import VaultArgumentParser, VaultConfig, VaultMongoClient, s_print
 from vaultlib import BColors as C
 from vaultlib.api import CPESchema, CVESchema
 
 
-def find_cve_id(cve_id: str) -> CVESchema | None:
+class VaultQuery:
     """
-    Returns CVE information given CVE ID.
-
-    :param cve_id: The CVE ID
-    :return: Dict of CVE.
+    Main class to query items from the Vault MongoDB
     """
-    return VAULT_MONGO.cves.find_one({"_id": cve_id})
+    def __init__(self, mongo_client: VaultMongoClient) -> None:
+        self.client = mongo_client
 
+    def find_cve_id(self, cve_id: str) -> CVESchema | None:
+        """
+        Returns CVE information given CVE ID.
 
-def find_cpe_id(cpe_id: str) -> CPESchema | None:
-    """
-    Returns CPE information given CPE ID.
+        :param cve_id: The CVE ID
+        :return: Dict of CVE.
+        """
+        return self.client.cves.find_one({"_id": cve_id})
 
-    :param cpe_id: The CPE ID
-    :return: Dict of CPE.
-    """
-    return VAULT_MONGO.cpes.find_one({"_id": cpe_id})
+    def find_cpe_id(self, cpe_id: str) -> CPESchema | None:
+        """
+        Returns CPE information given CPE ID.
 
+        :param cpe_id: The CPE ID
+        :return: Dict of CPE.
+        """
+        return self.client.cpes.find_one({"_id": cpe_id})
 
-def ml_find_cpe(
-        cpe_search_str: str,
-        frmt: str = "Vpv",
-        threshold: float = 80.0
-) -> list[tuple[float, CPESchema]]:
-    """
-    Using fuzzy matching, find the most similar CPEs
-    given a string containing the Vendor, Product, and/or Version
+    def ml_find_cpe(
+            self,
+            cpe_search_str: str,
+            frmt: str = "Vpv",
+            threshold: float = 80.0,
+            limit: int = 10
+    ) -> list[tuple[float, CPESchema]]:
+        """
+        Using fuzzy matching, find the most similar CPEs
+        given a string containing the Vendor, Product, and/or Version
 
-    :param cpe_search_str: String to search for
-    :param frmt: The specific ordering of token elements in the string.
-    V = Vendor, p = Product, v = Version.
-    :param threshold: WRatio score on
-    :return:
-    """
-    tokens = word_tokenize(cpe_search_str.lower())
-    matches = []
-    fetcher: tuple
-    match frmt:
-        case "Vpv":
+        :param cpe_search_str: String to search for
+        :param frmt: The specific ordering of token elements in the string.
+        V = Vendor, p = Product, v = Version.
+        :param threshold: Minimum WRatio score to be included in results.
+        :param limit: Maximum number of results to return.
+        :return: Sorted list of CPEs from highest WRatio score to lowest.
+        """
+        tokens = word_tokenize(cpe_search_str.lower())
+        weights = {"vendor": 0.4, "product": 0.4, "version": 0.2}
+        matches = []
+        get_weighted_score: Callable[[list[float]], float]
+        fetcher: tuple
+        if frmt == "Vpv":
+            def get_weighted_score(scores: list[float]) -> float:
+                return (
+                        (scores[0] * weights["vendor"]) +
+                        (scores[1] * weights["product"]) +
+                        (scores[2] * weights["version"])
+                )
+
             fetcher = (
                 (tokens[0], itemgetter("vendor")),
                 (tokens[1], itemgetter("product")),
                 (tokens[2], itemgetter("version")),
             )
-        case "pv":
+        elif frmt == "pv":
+            def get_weighted_score(scores: list[float]) -> float:
+                return (
+                        (scores[0] * weights["product"]) +
+                        (scores[1] * weights["version"])
+                )
+
             fetcher = (
                 (tokens[0], itemgetter("product")),
                 (tokens[1], itemgetter("version")),
             )
-        case _:
+        else:
             raise ValueError(f"frmt \"{frmt}\" is not supported")
-    for cpe in VAULT_MONGO.cpes.find({}):
-        match_scores = [WRatio(srch_str, db_itm_gttr(cpe)) for srch_str, db_itm_gttr in fetcher]
-        match_score = sum(match_scores) / len(match_scores)
-        if sum(match_scores) / len(match_scores) > threshold:
-            matches.append((match_score, cpe))
-    return matches
+
+        for cpe in self.client.cpes.find({}):
+            match_scores = [WRatio(srch_str, db_itm_gttr(cpe)) for srch_str, db_itm_gttr in fetcher]
+            if (score := get_weighted_score(match_scores)) > threshold:
+                matches.append((score, cpe))
+        matches.sort(key=itemgetter(0), reverse=True)
+        return matches[:limit]
 
 
 if __name__ == '__main__':
@@ -85,14 +109,16 @@ if __name__ == '__main__':
 
     config = VaultConfig(args.config)
     s_print("Connecting to MongoDB...")
-    VAULT_MONGO = VaultMongoClient(config).raise_if_not_connected()
+    mngo_client = VaultMongoClient(config).raise_if_not_connected()
     C.print_success("Connected.")
 
     s_print("Ensuring NLTK Model downloaded...")
     nltk.download(config.punkt_url)
     C.print_success("Complete.")
 
+    query = VaultQuery(mngo_client)
+
     if args.cve:
-        find_cve_id(args.cve)
+        print(query.find_cve_id(args.cve))
     elif args.cpesearch:
-        print(ml_find_cpe(args.cpesearch))
+        print(query.ml_find_cpe(args.cpesearch))
