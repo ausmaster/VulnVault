2383bacb9f45f460f6ced63e921051692220f60f
shart123456 <stephen.harttlieb@blacklanternsecurity.com>
2025-10-08 15:09:39 -0500

    Test for FAil 2

--- vulnvault/lib/api.py
+++ vulnvault/lib/api.py
@@ -390,7 +390,7 @@ class NVDFetch:  # pylint: disable=R0902
         :return: Serialized list of CPE matches.
         """
         cpe_matches = []
-        for match in res_cpe_matches:
+        for match in rest_cpe_matches:
             match = {
                 camel_to_snake(k): v
                 for k, v in match["matchString"].items()
@@ -455,6 +455,25 @@ class NVDFetch:  # pylint: disable=R0902
             **kwargs
         )
 
+@staticmethod
+def __serialize_cpe_matches(res_cpe_matches: list[dict[str, Any]]) -> list[dict[str, Any]]:
+    """
+    Serializes CPE matches to MongoDB digestable form.
+
+    :param res_cpe_matches: List of CPE matches returned from API.
+    :return: Serialized list of CPE matches.
+    """
+    cpe_matches = []
+    for match in res_cpe_matches:
+        match = {
+            camel_to_snake(k): v
+            for k, v in match["matchString"].items()
+        }
+        match["_id"] = match.pop("match_criteria_id")
+        if c_matches := match.get("matches"):
+            match["matches"] = [_match["cpeNameId"] for _match in c_matches]
+        cpe_matches.append(match)
+    return cpe_matches
 
 class NVDParallelAPICaller:  # pylint: disable=R0902
     """
